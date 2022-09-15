60b404cdac48333796ebc7ca8f6efbf3c77a22ad
PatricioMananA <p.manan@alumnos.santotomas.cl>
2022-09-14 21:30:46 -0300

    Miercoles 22:30

--- app/src/main/java/com/example/tomasinov3/Actividad3.java
+++ app/src/main/java/com/example/tomasinov3/Actividad3.java
@@ -1,13 +1,19 @@
 package com.example.tomasinov3;
 
+import android.support.constraint.ConstraintLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 
 public class Actividad3 extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+
+    ConstraintLayout Layout ;
+
 
     String [] country = {"seleccione sede","santiago","rancagua","la serena"};
     @Override
@@ -18,6 +24,7 @@ public class Actividad3 extends AppCompatActivity implements AdapterView.OnItemS
 
         Spinner spin = (Spinner) findViewById(R.id.spinner);
         spin.setOnItemSelectedListener(this);
+        Layout = findViewById(R.id.Layout);
 
 
         ArrayAdapter aa = new ArrayAdapter(this, android.R.layout.simple_spinner_item,country);
@@ -32,6 +39,18 @@ public class Actividad3 extends AppCompatActivity implements AdapterView.OnItemS
 
     @Override
     public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+
+
+    public void snack (View v2){
+        Snackbar.make(Layout,"Datos guardados correctamente", Snackbar.LENGTH_LONG).setAction("Cerrar", new View.OnClickListener() {
+            @Override
+            public void onClick(View v3) {
+
+            }
+        })
+                .setActionTextColor(getResources().getColor(R.color.purple_200)).show();
 
     }
 
